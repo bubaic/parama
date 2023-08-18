4f2ee3d3906b298775e2004a1893504a9e7a251f
Subrata <bubai@outlook.in>
2023-08-18 22:16:38 +0530

    added new icons

--- components/icon/index.ts
+++ components/icon/index.ts
@@ -2,14 +2,22 @@ import fb from "./fb.vue";
 import ig from "./ig.vue";
 import tw from "./tw.vue";
 import yt from "./yt.vue";
+import wa from "./wa.vue";
 import arrowup from "./arrow-up.vue";
 import arrowupright from "./arrow-up-right.vue";
+import mail from "./mail.vue";
+import phone from "./phone.vue";
+import map from "./map.vue";
 
 export {
   fb as IconFb,
   ig as IconIg,
   tw as IconTw,
   yt as IconYt,
+  wa as IconWa,
   arrowup as IconArrowUp,
   arrowupright as IconGo,
+  mail as IconMail,
+  map as IconMap,
+  phone as IconPhone,
 };
