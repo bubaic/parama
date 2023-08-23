1049b259a049e6bd8f60452646cb856c06fa3af3
Subrata <bubai@outlook.in>
2023-08-24 02:08:44 +0530

    added basic service page layout

--- composables/store.ts
+++ composables/store.ts
@@ -1,11 +1,61 @@
 const useStore = () => {
-  const defaultSiteTitle = "| Parama's Parlour",
-    openingStats = {
-      days: { from: "monday", to: "sunday" },
-      time: { from: "10:00", to: "07:30" },
-    };
+  const defaultSiteTitle = "| Parama's Parlour";
+  const openingStats = {
+    days: { from: "monday", to: "sunday" },
+    time: { from: "10:00", to: "07:30" },
+  };
+  const serviceList = [
+    {
+      name: "Nail arts & polishing",
+      imgSrc: "images/services/nail-art.svg",
+    },
+    {
+      name: "hair-cutting & dressing",
+      imgSrc: "images/services/hair-cutting.svg",
+    },
+    {
+      name: "bridal makeover",
+      imgSrc: "images/services/bridal-makeover.svg",
+    },
+    {
+      name: "spa & massage",
+      imgSrc: "images/services/spa.svg",
+    },
+    {
+      name: "threading & waxing",
+      imgSrc: "images/services/waxing.svg",
+    },
+    {
+      name: "manicure",
+      imgSrc: "images/services/manicure.svg",
+    },
+    {
+      name: "skincare & treatments",
+      imgSrc: "images/services/skin-treatments.svg",
+    },
+    {
+      name: "pedicure",
+      imgSrc: "images/services/pedicure.svg",
+    },
+    {
+      name: "make-up essentials",
+      imgSrc: "images/services/makeup.svg",
+    },
+    {
+      name: "hair treatments & coloring",
+      imgSrc: "images/services/hair-color.svg",
+    },
+    {
+      name: "grooming & makeover",
+      imgSrc: "images/services/makeover.svg",
+    },
+  ];
 
-  return { defaultSiteTitle, openingStats };
+  return {
+    defaultSiteTitle,
+    openingStats,
+    serviceList,
+  };
 };
 
 export { useStore };
--- pages/services.vue
+++ pages/services.vue
@@ -2,19 +2,7 @@
 import { IconGo } from "@/components/icon";
 import { useStore } from "@/composables/store";
 
-const lists = [
-  "Nail arts & polish",
-  "hair cutting & dressing",
-  "Spa & massage",
-  "Hair treatments & Coloring",
-  "Bridal makeover",
-  "Bridal makeover",
-  "Bridal makeover",
-  "Bridal makeover",
-  "Bridal makeover",
-  "Bridal makeover",
-  "Bridal makeover",
-];
+const { serviceList } = useStore();
 
 const { defaultSiteTitle } = useStore();
 useSeoMeta({ title: `Services we offer ${defaultSiteTitle}` });
@@ -23,64 +11,83 @@ useSeoMeta({ title: `Services we offer ${defaultSiteTitle}` });
 <template>
   <div class="page">
     <h1>What we can offer you</h1>
+
     <section class="cards">
-      <div v-for="item in lists" class="card">
-        <div class="card__img"></div>
-        <div class="card--content">
-          <div class="card__text">{{ item }}</div>
-          <div class="icon-wrapper"><IconGo class="w-5" /></div>
+      <button @click="" v-for="item in serviceList" class="card">
+        <div class="card__img">
+          <img
+            class="h-full w-full"
+            :src="item.imgSrc"
+            :alt="'image of ' + item.name"
+          />
         </div>
-      </div>
+
+        <span class="card--content">
+          <div class="card__text">{{ item.name }}</div>
+          <div class="icon-wrapper">
+            <IconGo class="icon" />
+          </div>
+        </span>
+      </button>
     </section>
   </div>
 </template>
 
 <style lang="scss" scoped>
-@use "../assets/style/_utils";
+@import "../assets/style/mixins";
 
 .page {
   @apply container mx-auto xl:max-w-[1510px];
 
   h1 {
-    @apply px-4 mt-2 text-3xl sm:hidden;
+    @include visually-hidden;
   }
 }
 
 .cards {
-  @apply my-5 w-full grid p-4 gap-6 grid-cols-2 justify-items-center
-    items-start sm:gap-0 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6
-    xl:gap-12;
+  @apply mt-2 mb-5 w-full grid gap-6 grid-cols-2 justify-items-center
+    items-start sm:gap-0 sm:grid-cols-3 <sm:px-4 lg:grid-cols-4
+    xl:gap-12 xl:grid-cols-6;
 }
 
 .card {
-  transition: all 200ms ease-out;
-  @apply vstack sm:mb-6 gap-3 sm:gap-4 cursor-pointer;
+  transition: all 250ms ease-out;
+  @apply outline-none gap-2 vstack sm:mb-6 sm:gap-4;
 
   &__img {
-    transition: filter 200ms ease, background-color 300ms ease-out;
-    @extend %gradient-white;
-    @apply border rounded-xl to-[#ECE2DF] border-brand-accent/80
-      h-40 w-40 sm:h-50 sm:w-50;
+    @apply bg-platinum border border-brand-background rounded-xl h-40
+      p-5 transition-all ease-out w-40 duration-200 filter
+      drop-shadow-large vstack sm:h-50 sm:p-10 sm:w-50;
   }
 
   &:hover,
   &:focus {
     .card__img {
-      @extend %gradient-royal-white, %drop-shadow;
+      @apply bg-lavendar-alabaster border-brand-accent drop-shadow-small;
+    }
+
+    .icon {
+      @apply bg-brand-accent fill-brand-background p-0.75;
+
+      & {
+        @apply rounded-full;
+      }
     }
   }
 
   &--content {
-    @apply w-full hstack justify-between sm:max-w-50;
+    @apply cursor-pointer w-full px-1 gap-2 hstack items-start
+      justify-between sm:max-w-50;
   }
 
   &__text {
-    @apply text-sm sm:text-base sm:leading-tight max-w-30;
+    @apply text-sm text-brand-secondary-alt text-left leading-tight
+      lowercase sm:text-base sm:leading-[1.15] first-letter:capitalize
+      pl-1;
   }
 }
 
-.icon-wrapper {
-  @apply rounded-md;
-  @extend %gradient-white;
+.icon {
+  @apply h-5 fill-brand-secondary-alt w-5;
 }
 </style>
