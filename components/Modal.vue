2055130ece1394f9e699d5d31f891d8f30037728
Subrata <bubai@outlook.in>
2023-09-05 18:07:13 +0530

    fix: slide-up transition in modal

--- components/Modal.vue
+++ components/Modal.vue
@@ -1,16 +1,43 @@
 <script setup lang="ts">
-const { isModalOpen } = defineProps(["isModalOpen"]);
+import { PropType } from ".nuxt/imports";
+import { MediaType } from "types";
+
+const { isModalOpen, mediaItem } = defineProps({
+  isModalOpen: { type: Boolean },
+  mediaItem: { type: Object as PropType<MediaType>, default: {} },
+});
+
+function getMediaItemType(source: string) {
+  const imageTypes = /(jpg|jpeg|png|webp|avip)/g;
+
+  return imageTypes.test(source.split(".")[1]) ? "image" : "video";
+}
 </script>
 
 <template>
   <Teleport to="body">
     <div class="wrapper" v-if="isModalOpen">
       <div class="modal">
-        <p>Hello from the modal!</p>
-        <button @click="isModalOpen = false">Close</button>
+        <div class="modal__media">
+          <img
+            v-if="getMediaItemType(mediaItem.src) == 'image'"
+            :src="mediaItem.src"
+            :alt="'image of ' + mediaItem.title"
+          />
+          <video v-else preload="metadata" controls autoplay>
+            <source :src="mediaItem.src" />
+          </video>
+        </div>
+
+        <h3>{{ mediaItem.title }}</h3>
+        <p>{{ mediaItem.details }}</p>
+
+        <button class="close" @click="isModalOpen = !isModalOpen">
+          <IconClose class="w-7" />
+        </button>
       </div>
 
-      <div class="bg" @click="isModalOpen = false" />
+      <div class="bg" @click="isModalOpen = !isModalOpen" />
     </div>
   </Teleport>
 </template>
@@ -19,12 +46,49 @@ const { isModalOpen } = defineProps(["isModalOpen"]);
 .wrapper {
   @apply grid inset-0 z-9999 fixed place-items-center select-none;
 }
+
 .modal {
-  @apply bg-white p-4 transform z-1 -translate-y-1/2 filter 
-    drop-shadow-large;
+  animation: slideUp 250ms ease-out forwards;
+  @apply bg-brand-background rounded-lg text-brand-primary top-1/2 z-1
+    filter drop-shadow-large absolute overflow-hidden <sm:mx-4;
+
+  img,
+  video {
+    @apply rounded-t-lg max-h-screen-sm object-cover h-[calc(100vh-10rem)]
+      w-full;
+  }
+
+  p:not(:empty),
+  h3:not(:empty) {
+    @apply p-3 lowercase first-letter:capitalize;
+  }
+  h3 {
+    @apply font-semibold font-sans text-xl;
+  }
+
+  .close {
+    @apply rounded-full bg-brand-primary/50 text-white p-1 top-2
+      right-2 absolute;
+
+    &:hover,
+    &:focus,
+    &:active {
+      @apply bg-brand-primary;
+    }
+  }
+}
+@keyframes slideUp {
+  from {
+    opacity: 0;
+  }
+  to {
+    transform: translateY(-50%);
+    opacity: 1;
+  }
 }
 
 .bg {
-  @apply bg-dark-800/80 inset-0 absolute filter backdrop-blur-lg;
+  @apply bg-brand-muted/50 inset-0 absolute backdrop-filter
+    backdrop-blur-[2px];
 }
 </style>
